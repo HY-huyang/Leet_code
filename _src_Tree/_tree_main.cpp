a3ae8bf1ca77d47839e57f4d790698abd92fd93f
HY-huyang <155282732+HY-huyang@users.noreply.github.com>
2025-12-13 13:41:44 +0800

    Update _tree_main.cpp

--- _src_Tree/_tree_main.cpp
+++ _src_Tree/_tree_main.cpp
@@ -1,4 +1,4 @@
-//94_1  递归  _2  stack
+// 94_1  递归  _2  stack
 int main() {
     vector<int>ve = { 1,2,3,4,5 };
     ListNode* ls = to_list(ve);
@@ -6,11 +6,20 @@ int main() {
     return 0;
 }
 
-//0102 二叉树的层序遍历
+// 0102 二叉树的层序遍历
 int main() {
     vector<optional<int>>vec = { 3,9,20,nullopt,nullopt,15,7 };
     TreeNode* tree = to_tree(vec);
     vector<vector<int>>num = Solution().levelOrder(tree);
     cout << num;
+    return 0;
+}
+
+// 0112 路径总和
+int main() {
+    vector<optional<int>>vec = { 5,4,8,11,nullopt,13,4,7,2,nullopt,nullopt,nullopt,1 };
+    int k = 22;
+    TreeNode* tree = to_tree(vec);
+    Solution().hasPathSum(tree, k);
     return 0;
 }
