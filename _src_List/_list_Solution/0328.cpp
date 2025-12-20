3e42511db9456c6485648418ac1210a88e1ec293
HY-huyang <155282732+HY-huyang@users.noreply.github.com>
2025-12-20 10:14:15 +0800

    Update 0328.cpp

--- _src_List/_list_Solution/0328.cpp
+++ _src_List/_list_Solution/0328.cpp
@@ -1,3 +1,4 @@
+// 1 在原链表上进行节点移动
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
@@ -20,3 +21,7 @@ public:
         return fhead->next;
     }
 };
+
+// 2 创建两个新节点，ListNode* odd_node,后面跟head的奇数节点
+                    ListNode* even_node，后面跟head的偶数节点
+最后将两个新链表合并
